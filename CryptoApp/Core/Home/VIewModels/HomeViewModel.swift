b70c07ff6971e7f0418fbaf5257a574bc07fdc05
NazarPrysiazhnyi <2008Nazar@gmail.com>
2023-03-08 11:19:38 +0200

    feat [SearchBarView]: added SearchBarView

--- CryptoApp/Core/Home/VIewModels/HomeViewModel.swift
+++ CryptoApp/Core/Home/VIewModels/HomeViewModel.swift
@@ -13,6 +13,8 @@ class HomeViewModel: ObservableObject {
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     
+    @Published var searchText: String = ""
+    
     private let dataService = CoinDataService()
     private var cancellables = Set<AnyCancellable>()
     
@@ -22,10 +24,25 @@ class HomeViewModel: ObservableObject {
     
     func addSubscribers() {
         
-        dataService.$allCoins
+        // update allCoins
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .milliseconds(500) , scheduler: RunLoop.main)
+            .map(filterCoins)
             .sink { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+    }
+    
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else { return coins }
+        
+        let lowercasedText = text.lowercased()
+        return coins.filter { coin in
+            return coin.name.contains(lowercasedText)
+            || coin.symbol.contains(lowercasedText)
+            || coin.id.contains(lowercasedText)
+        }
     }
 }
